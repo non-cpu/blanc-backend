77d3a8ccdd5b0fcd3f8e07c27cfab6ddd8fd719a
non <grapher285@gmail.com>
2023-10-15 11:30:41 +0900

    init

---
+++ src/test/java/com/blanc/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blanc.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
