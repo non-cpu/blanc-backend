f0c5c490d54cd1b93fb099e2b103a1158dc6ed44
DongWon <ehddnjs1747@daum.net>
2023-11-20 15:18:48 +0900

    feat: order swagger 설명 추가

--- src/main/java/com/blanc/market/domain/order/controller/OrderController.java
+++ src/main/java/com/blanc/market/domain/order/controller/OrderController.java
@@ -4,6 +4,8 @@ package com.blanc.market.domain.order.controller;
 import com.blanc.market.domain.order.dto.OrderRequest;
 import com.blanc.market.domain.order.dto.OrderResponse;
 import com.blanc.market.domain.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,11 +16,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Order", description = "Order API Document")
 public class OrderController {
 
     private final OrderService orderService;
 
-
+    @Operation(summary = "주문 생성", description = "주문 생성 메서드입니다.")
     @PostMapping("/api/orders")
     public ResponseEntity<OrderResponse> createOrder(
             @RequestBody @Valid OrderRequest dto){
@@ -28,6 +31,7 @@ public class OrderController {
     }
 
     //주문 상태 취소로 변경
+    @Operation(summary = "주문 취소", description = "주문 취소 메서드입니다. 주문 상태를 cancle로 변경합니다.")
     @PostMapping("api/orders/{id}/cancle")
     public ResponseEntity<Void> cancleOrders(@PathVariable Long id){
         orderService.cancleOrders(id);
@@ -38,6 +42,7 @@ public class OrderController {
 
 
     //사용자 주문 조회
+    @Operation(summary = "현재 사용자 주문 조회", description = "주문 조회 메서드입니다. 현재 사용자의 주문 리스트를 조회합니다.")
     @GetMapping("api/orders/my-orders")
     public ResponseEntity<List<OrderResponse>> getMyOrders(){
         return ResponseEntity.ok(orderService.findAllMyOrders());
