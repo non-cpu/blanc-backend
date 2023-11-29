2fc47c198ad7436931dbf1edd745181714528e00
non <grapher285@gmail.com>
2023-11-29 19:31:56 +0900

    feat: like 기능 수정 및 추가

--- src/main/java/com/blanc/market/domain/like/entity/Like.java
+++ src/main/java/com/blanc/market/domain/like/entity/Like.java
@@ -5,13 +5,11 @@ import com.blanc.market.domain.user.entity.User;
 import com.blanc.market.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.Where;
 
 @Entity
 @Getter
 @Builder
 @Table(name = "likes")
-@Where(clause = "is_deleted = false")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Like extends BaseEntity {
